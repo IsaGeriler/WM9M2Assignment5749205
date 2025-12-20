e82abe496d0c5a2c4c355f6721aecda205c500ea
Isa Berk Geriler <gerilerisaberk@gmail.com>
2025-12-20 16:16:28 +0000

    Update: Roughness Mapping Attempted...

--- WM9M2Assignment5749205/StaticModel.h
+++ WM9M2Assignment5749205/StaticModel.h
@@ -14,7 +14,7 @@ public:
 	std::vector<Mesh*> meshes;
 	std::vector<std::string> albedoFilenames;
 	std::vector<std::string> normalFilenames;
-	// std::vector<std::string> roughnessFilenames;
+	std::vector<std::string> roughnessFilenames;
 	
 	void load(Core* core, PSOManager* psos, TextureManager* textures, ShaderManager* shaders, std::string filename) {
 		GEMLoader::GEMModelLoader loader;
@@ -32,11 +32,11 @@ public:
 			// Load texture with filename: gemmeshes[i].material.find("albedo").getValue()
 			albedoFilenames.push_back(gemmeshes[i].material.find("albedo").getValue());
 			normalFilenames.push_back(gemmeshes[i].material.find("nh").getValue());
-			// roughnessFilenames.push_back(gemmeshes[i].material.find("rmax").getValue());
+			roughnessFilenames.push_back(gemmeshes[i].material.find("rmax").getValue());
 			
 			textures->loadTexture(core, albedoFilenames[i], gemmeshes[i].material.find("albedo").getValue());
 			textures->loadTexture(core, normalFilenames[i], gemmeshes[i].material.find("nh").getValue());
-			// textures->loadTexture(core, roughnessFilenames[i], gemmeshes[i].material.find("rmax").getValue());
+			textures->loadTexture(core, roughnessFilenames[i], gemmeshes[i].material.find("rmax").getValue());
 			
 			mesh->initialize(core, vertices, gemmeshes[i].indices);
 			meshes.push_back(mesh);
@@ -54,14 +54,14 @@ public:
 		for (int i = 0; i < meshes.size(); i++) {
 			std::cout << albedoFilenames[i] << ' ' << textures->find(albedoFilenames[i]) << '\n';
 			std::cout << normalFilenames[i] << ' ' << textures->find(normalFilenames[i]) << '\n';
-			// std::cout << roughnessFilenames[i] << ' ' << textures->find(roughnessFilenames[i]) << '\n';
+			std::cout << roughnessFilenames[i] << ' ' << textures->find(roughnessFilenames[i]) << '\n';
 
 			// Normal map can be found in gemmeshes[i].material.find("nh").getValue()
 			// The rgb channels contain the normal map, and the alpha channel contains a height map
 
 			shaders->updateTexturePS(core, "StaticModelTextured", "albedoTexture", textures->find(albedoFilenames[i]));
 			shaders->updateTexturePS(core, "StaticModelTextured", "normalsTexture", textures->find(normalFilenames[i]));
-			// shaders->updateTexturePS(core, "StaticModelTextured", "roughnessTexture", textures->find(roughnessFilenames[i]));
+			shaders->updateTexturePS(core, "StaticModelTextured", "roughnessTexture", textures->find(roughnessFilenames[i]));
 			
 			meshes[i]->draw(core);
 		}
@@ -75,6 +75,7 @@ public:
 
 	std::vector<std::string> albedoFilenames;
 	std::vector<std::string> normalFilenames;
+	std::vector<std::string> roughnessFilenames;
 
 	std::string shadername;
 	std::string psoname;
@@ -96,9 +97,11 @@ public:
 			// Load texture with filename: gemmeshes[i].material.find("albedo").getValue()
 			albedoFilenames.push_back(gemmeshes[i].material.find("albedo").getValue());
 			normalFilenames.push_back(gemmeshes[i].material.find("nh").getValue());
+			roughnessFilenames.push_back(gemmeshes[i].material.find("rmax").getValue());
 
 			textures->loadTexture(core, albedoFilenames[i], gemmeshes[i].material.find("albedo").getValue());
 			textures->loadTexture(core, normalFilenames[i], gemmeshes[i].material.find("nh").getValue());
+			textures->loadTexture(core, roughnessFilenames[i], gemmeshes[i].material.find("rmax").getValue());
 			
 			mesh->initialize(core, vertices, gemmeshes[i].indices, instances);
 			meshes.push_back(mesh);
@@ -117,12 +120,14 @@ public:
 		for (int i = 0; i < meshes.size(); i++) {
 			std::cout << albedoFilenames[i] << ' ' << textures->find(albedoFilenames[i]) << '\n';
 			std::cout << normalFilenames[i] << ' ' << textures->find(normalFilenames[i]) << '\n';
+			std::cout << roughnessFilenames[i] << ' ' << textures->find(roughnessFilenames[i]) << '\n';
 
 			// Normal map can be found in gemmeshes[i].material.find("nh").getValue()
 			// The rgb channels contain the normal map, and the alpha channel contains a height map
 
 			shaders->updateTexturePS(core, shadername, "albedoTexture", textures->find(albedoFilenames[i]));
 			shaders->updateTexturePS(core, shadername, "normalsTexture", textures->find(normalFilenames[i]));
+			shaders->updateTexturePS(core, shadername, "roughnessTexture", textures->find(roughnessFilenames[i]));
 			meshes[i]->draw(core);
 		}
 	}
